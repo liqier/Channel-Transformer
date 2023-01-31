cd4f2cf39c4293da24dd2b4239a71ceda8e9a98f
liqier <55725190+liqier@users.noreply.github.com>
2023-01-31 10:22:22 +0800

    Add files via upload

---
+++ feeders/feeder_animal.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+from torch.utils.data import Dataset
+
+from feeders import tools
+import pickle
+
+
+class Feeder(Dataset):
+    def __init__(self,
+                 data_path,
+                 label_path,
+                 random_choose=False,
+                 random_shift=False,
+                 random_move=False,
+                 random_rot=False,
+                 window_size=-1,
+                 normalization=False,
+                 debug=False,
+                 use_mmap=False,
+                 bone=False,
+                 vel=False):
+        """
+        :param data_path:
+        :param label_path:
+        :param split: training set or test set
+        :param random_choose: If true, randomly choose a portion of the input sequence
+        :param random_shift: If true, randomly pad zeros at the begining or end of sequence
+        :param random_move:
+        :param random_rot: rotate skeleton around xyz axis
+        :param window_size: The length of the output sequence
+        :param normalization: If true, normalize input sequence
+        :param debug: If true, only use the first 100 samples
+        :param use_mmap: If true, use mmap mode to load data, which can save the running memory
+        :param bone: use bone modality or not
+        :param vel: use motion modality or not
+        :param only_label: only load label for ensemble score compute
+        """
+
+        self.debug = debug
+        self.data_path = data_path
+        self.label_path = label_path
+        self.random_choose = random_choose
+        self.random_shift = random_shift
+        self.random_move = random_move
+        self.window_size = window_size
+        self.normalization = normalization
+        self.use_mmap = use_mmap
+        self.random_rot = random_rot
+        self.bone = bone
+        self.vel = vel
+        self.load_data()
+        if normalization:
+            self.get_mean_map()
+
+    def load_data(self):
+        # data: N C V T M
+
+        # load label
+        if '.pkl' in self.label_path:
+            try:
+                with open(self.label_path) as f:
+                    self.sample_name, self.label = pickle.load(f)
+            except:
+                # for pickle file from python2
+                with open(self.label_path, 'rb') as f:
+                    self.sample_name, self.label = pickle.load(
+                        f, encoding='latin1')
+
+        # old label format
+        elif '.npy' in self.label_path:
+            self.label = list(np.load(self.label_path))
+            self.sample_name = [str(i) for i in range(len(self.label))]
+        else:
+            raise ValueError()
+
+        # load data
+        self.data = np.load(self.data_path)
+
+    def get_mean_map(self):
+        data = self.data
+        N, C, T, V, M = data.shape
+        self.mean_map = data.mean(axis=2, keepdims=True).mean(axis=4, keepdims=True).mean(axis=0)
+        self.std_map = data.transpose((0, 2, 4, 1, 3)).reshape((N * T * M, C * V)).std(axis=0).reshape((C, 1, V, 1))
+
+    def __len__(self):
+        return len(self.label)
+
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, index):
+        # get data
+        data_numpy = np.array(self.data[index])
+        label = self.label[index]
+        sample=self.sample_name[index]
+
+        # normalization
+        if self.normalization:
+            data_numpy = (data_numpy - self.mean_map) / self.std_map
+        # processing
+        if self.random_shift:
+            data_numpy = tools.random_shift(data_numpy)
+        if self.random_choose:
+            data_numpy = tools.random_choose(data_numpy, self.window_size)
+        elif self.window_size > 0:
+            data_numpy = tools.auto_pading(data_numpy, self.window_size)
+        if self.random_move:
+            data_numpy = tools.random_move(data_numpy)
+
+        return data_numpy, label, sample
+
+    def top_k(self, score, top_k):
+        rank = score.argsort()
+        #print(type(rank))#<class 'numpy.ndarray'>
+        #print(type(self.label))#<class 'list'>
+
+        hit_top_k = [l in rank[i, -top_k:] for i, l in enumerate(np.array(self.label))]
+        return sum(hit_top_k) * 1.0 / len(hit_top_k)
+
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
